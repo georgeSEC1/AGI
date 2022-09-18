a4b26b479818229b88faa68d61289e278f3a0ca4
georgeSEC1 <111563036+georgeSEC1@users.noreply.github.com>
2022-09-18 14:49:52 +1000

    Add files via upload

--- kernel.py
+++ kernel.py
@@ -1,4 +1,5 @@
-def returnmethods(base, code):
+import random
+def returnMethods(base, code):
     method = ""
     n = 0
     while( n < len(code)):
@@ -10,6 +11,17 @@ def returnmethods(base, code):
             method += "\n"
         n += 1
     return method + ""
+def returnRandomMethods(base, code):
+    method = "try:\n\t"
+    n = 0
+    while(n < 100):
+        var = random.randint(0,len(base))
+        if var == len(base):
+            method += "\n"
+        if var != len(base) and 29 != var and 30 != var:
+            method += base[var].strip() + " "
+        n += 1
+    return method + "\nexcept:\n\tprint(\"details...\")"
 with open("carrier.conf", encoding='ISO-8859-1') as f:
     carrier = f.read()
 with open("base.conf", encoding='ISO-8859-1') as f:
@@ -17,7 +29,8 @@ with open("base.conf", encoding='ISO-8859-1') as f:
 with open("manifest.conf", encoding='ISO-8859-1') as f:
     code = f.readlines()
     f = open("respondent.py", "w", encoding="utf8")
-    proc = returnmethods(base,code)
+    proc = returnMethods(base,code)
+    proc = returnRandomMethods(base,code)
     f.write(carrier)
     f.write(proc.strip())
     f.flush
